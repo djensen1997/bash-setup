3e2627a515711d8eacf51f9c5dac74c96b47157e
Dane Jensen <firstlegoboy@comcast.net>
2024-11-15 13:33:28 -0600

    updated setup for linux

--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
-BASE_DIR="( dirname $0 )"
+BASE_DIR="$( dirname $0 )"
 DIR="$(pwd)"
-FILE=$BASE_DIR/DIR/source_me
+FILE=$DIR/source_me
 
-if [ $# -ne 0 ]; then
+if [ $# -ne 1 ]; then
     PROFILE=.bash_profile
 else
     PROFILE=$1
 fi
 
+echo "BASE_DIR $BASE_DIR"
+echo "DIR $DIR"
+echo "FILE $FILE"
+echo "PROFILE $PROFILE"
+
 echo "Adding RC to the bash profile"
-echo "#Dane's Bash Functions" >> $HOME/$PROFILE
+echo "\n#Dane's Bash Functions" >> $HOME/$PROFILE
 echo "source $FILE" >> $HOME/$PROFILE
 echo "Done with setup, restart bash to get functionality"
